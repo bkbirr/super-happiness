d12928fa8d13e720be90b0a54914e31259a925ec
Erin Claudio <eclaudio@gmail.com>
2019-12-17 10:33:53 -0500

    homepage added with some html

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=%7jav99#+(i3t1b+ifdzaskrt7#ou_z@np*a8gsy-fgh#5l5c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('RENDER_EXTERNAL_HOSTNAME', default='')]
+ALLOWED_HOSTS = [os.getenv('RENDER_EXTERNAL_HOSTNAME', default=''),('127.0.0.1')]
 
 
 # Application definition
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'project/templates')
+                ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from projects.models import Project
 from django.http import HttpResponse
 
-def project_list(request):
-    return HttpResponse("<h1> Hello world portfolio</h1>")
+
+
+def all_projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html',{'projects':projects})
+
+def project_detail(request,pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html',
+                  {'project': project})
+
+
 
 
