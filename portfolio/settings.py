8f2630397882a610a7cf1a7551771dfff7aa7d7f
Brenton Birr <bkbirr@msn.com>
2020-01-02 11:07:11 -0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,10 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), '/opt/render/project/src/static' , ]
-STATIC_ROOT = "/opt/render/project/src/static"
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
